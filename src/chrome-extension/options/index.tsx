5c1d5deb19f3f64b2e954621b505e82d971cd543
Anjola Adeyemi <anjolaadeyemi4545@gmail.com>
2024-12-12 23:18:11 +0100

    extension

--- src/chrome-extension/options/index.tsx
+++ src/chrome-extension/options/index.tsx
@@ -1,9 +1,10 @@
-import "../global.css";
+import React from "react";
 
 const Options = () => {
   return (
-    <div className="text-5xl p-10 font-extrabold">
-      <div>This is your options page.</div>
+    <div className="p-4 w-80">
+      <h1 className="text-lg font-bold">MACI Key Manager Settings</h1>
+      <p>Customize your key management experience.</p>
     </div>
   );
 };
